294778116fb0a94a8c6f7acdd2657207743e3923
Levi Asmussen <leviasmussen@gmail.com>
2026-04-19 20:42:56 +0200

    feat(person): add delete endpoint for person

---
+++ application/src/main/kotlin/com/alleslocker/backend/application/person/usecase/DeletePersonUseCase.kt
@@ -0,0 +1,7 @@
+﻿package com.alleslocker.backend.application.person.usecase
+
+import com.alleslocker.backend.application.common.InputBoundary
+import com.alleslocker.backend.application.person.dto.request.DeletePersonRequestDto
+import com.alleslocker.backend.application.person.dto.response.DeletePersonResponseDto
+
+interface DeletePersonUseCase : InputBoundary<DeletePersonRequestDto, DeletePersonResponseDto>
