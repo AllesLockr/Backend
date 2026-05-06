776c2a2e7e69912e6773854592fdf6cc9b9d8aa5
Luca Schumacher <l.schumacher@luftiger.eu>
2026-05-06 21:26:36 +0200

    feature(adapter): implement first approach

---
+++ application/src/main/kotlin/com/alleslocker/backend/application/person/adapter/PersonAdapter.kt
@@ -0,0 +1,7 @@
+package com.alleslocker.backend.application.person.adapter
+
+import com.alleslocker.backend.application.common.adapter.Adapter
+
+interface PersonAdapter : Adapter {
+    fun addPerson()
+}
---
+++ lockConnector/src/main/kotlin/com/alleslocker/backend/lockconnector/person/adapter/PersonAdapterImpl.kt
@@ -0,0 +1,12 @@
+package com.alleslocker.backend.lockconnector.person.adapter
+
+import com.alleslocker.backend.application.person.adapter.PersonAdapter
+import org.springframework.stereotype.Component
+
+@Component
+internal class PersonAdapterImpl : PersonAdapter {
+    override fun addPerson() {
+        TODO("Not yet implemented")
+    }
+
+}
