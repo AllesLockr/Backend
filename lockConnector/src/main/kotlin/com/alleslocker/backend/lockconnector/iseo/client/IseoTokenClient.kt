83e87a3c1d4367807cb0c9bb281335a410eaddf8
Levi Asmussen <leviasmussen@gmail.com>
2026-05-21 12:44:42 +0200

    feat(adapter): add Iseo configuration and token management classes

---
+++ lockConnector/src/main/kotlin/com/alleslocker/backend/lockconnector/iseo/client/IseoTokenClient.kt
@@ -0,0 +1,28 @@
+﻿package com.alleslocker.backend.lockconnector.iseo.client
+
+import com.alleslocker.backend.lockconnector.iseo.config.IseoConfig
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.client.RestClient
+
+@Component
+class IseoTokenClient(private val config: IseoConfig) {
+    private val client by lazy {
+        RestClient.builder().baseUrl(config.baseUrl).build()
+    }
+
+    fun getToken(): IseoTokenResponse {
+        val formData = LinkedMultiValueMap<String, String>().apply{
+            add("grant_type", "password")
+            add("username", config.username)
+            add("password", config.password)
+        }
+        return client.post().uri("/oauth/token")
+            .headers { it.setBasicAuth(config.clientId, config.clientSecret) }
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(formData)
+            .retrieve()
+            .body(IseoTokenResponse::class.java) ?: throw IllegalStateException("Failed to get IseoTokenResponse")
+    }
+}
