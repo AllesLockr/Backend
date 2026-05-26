19ef46492742c71e7f6b82d70eeebf13a5caed21
Leviasm <leviasmussen@gmail.com>
2026-05-26 14:26:20 +0200

    feat(person): implement IseoPersonClient for adding and deleting users on ISEO Api

--- lockConnector/src/main/kotlin/com/alleslocker/backend/lockconnector/person/client/IMoqPersonClientImpl.kt
+++ lockConnector/src/main/kotlin/com/alleslocker/backend/lockconnector/person/client/IMoqPersonClientImpl.kt
@@ -2,13 +2,14 @@ package com.alleslocker.backend.lockconnector.person.client
 
 import com.alleslocker.backend.application.person.dto.request.AddPersonAdapterRequest
 import com.alleslocker.backend.application.person.dto.request.DeletePersonAdapterRequest
+import com.alleslocker.backend.application.person.dto.response.AddPersonAdapterResponse
 import com.alleslocker.backend.lockconnector.person.adapter.PersonClient
 import com.alleslocker.backend.lockconnector.rest.GenericRestClient
 
 class IMoqPersonClientImpl(private val restClient: GenericRestClient) : PersonClient {
     data class RoleResponse(val id: Int, val name: String)
 
-    override fun addPerson(request: AddPersonAdapterRequest) {
+    override fun addPerson(request: AddPersonAdapterRequest): AddPersonAdapterResponse {
         //TODO configure URL in a more central place or read from db?
         val availableRoles: List<RoleResponse>? = restClient.get("http://localhost:8089/api/v2/roles")
 
@@ -20,6 +21,7 @@ class IMoqPersonClientImpl(private val restClient: GenericRestClient) : PersonCl
         )
 
         restClient.post("http://localhost:8089/api/v2/users", transformedRequest)
+        return AddPersonAdapterResponse(id = null)
     }
 
     override fun deletePerson(request: DeletePersonAdapterRequest) {
