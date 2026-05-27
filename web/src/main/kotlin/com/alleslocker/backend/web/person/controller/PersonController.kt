8fc09701b8d0cb3dda2dae7cba60ab18abe769b4
Luca <l.schumacher@luftiger.eu>
2026-05-27 10:56:58 +0200

    refactor(person): remove unused import for GetMapping in PersonController

--- web/src/main/kotlin/com/alleslocker/backend/web/person/controller/PersonController.kt
+++ web/src/main/kotlin/com/alleslocker/backend/web/person/controller/PersonController.kt
@@ -22,7 +22,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
