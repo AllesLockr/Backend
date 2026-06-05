ee1d5da51504b58a81ceab74eeebb3389d8f0795
Luca <l.schumacher@luftiger.eu>
2026-06-05 21:55:43 +0200

    feat(service): enhance PasswordGeneratorService to use SecureRandom for password generation

--- application/src/main/kotlin/com/alleslocker/backend/application/common/service/PasswordGeneratorService.kt
+++ application/src/main/kotlin/com/alleslocker/backend/application/common/service/PasswordGeneratorService.kt
@@ -1,8 +1,12 @@
 package com.alleslocker.backend.application.common.service
 
+import java.security.SecureRandom
+
 class PasswordGeneratorService {
+    private val secureRandom = SecureRandom()
+
     fun generate(length: Int = 8): String {
         val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+"
-        return (1..length).map { chars.random() }.joinToString("")
+        return (1..length).map { chars[secureRandom.nextInt(chars.length)] }.joinToString("")
     }
 }
