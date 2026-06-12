8c3131a2258da1d07e2163a6937d0696f3d871e0
Luca <l.schumacher@luftiger.eu>
2026-06-12 13:40:39 +0200

    feat(user): implement change user role use case

---
+++ application/src/main/kotlin/com/alleslocker/backend/application/user/usecase/ChangeUserRoleUseCaseImpl.kt
@@ -0,0 +1,103 @@
+package com.alleslocker.backend.application.user.usecase
+
+import com.alleslocker.backend.application.common.ErrorResponse
+import com.alleslocker.backend.application.common.Logger
+import com.alleslocker.backend.application.common.OutputBoundary
+import com.alleslocker.backend.application.user.dto.request.ChangeUserRoleRequestDto
+import com.alleslocker.backend.application.user.gateway.UserGateway
+import com.alleslocker.backend.application.user.mapper.toDomain
+import com.alleslocker.backend.application.user.mapper.toDto
+import com.alleslocker.backend.domain.auditlog.AuditLog
+import com.alleslocker.backend.domain.auditlog.AuditLogId
+import com.alleslocker.backend.domain.auditlog.AuditLogMessage
+import com.alleslocker.backend.domain.user.UserId
+import com.alleslocker.backend.domain.user.UserRole
+import java.time.Instant
+
+class ChangeUserRoleUseCaseImpl(
+    private val userGateway: UserGateway,
+    private val logger: Logger,
+) : ChangeUserRoleUseCase {
+    override fun execute(
+        request: ChangeUserRoleRequestDto,
+        presenter: OutputBoundary<Unit>
+    ) {
+        val requestorId =
+            try {
+                UserId(request.userId)
+            } catch (e: IllegalArgumentException) {
+                presenter.presentFailure(ErrorResponse.BadRequest("Invalid id: ${e.message}"))
+                return
+            }
+
+        val requestorUser =
+            try {
+                userGateway.findById(requestorId)
+            } catch (e: Exception) {
+                presenter.presentFailure(ErrorResponse.InternalServerError("Failed to load user"))
+                logger.error("Failed to load user", e)
+                return
+            }
+
+        if (requestorUser == null) {
+            presenter.presentFailure(ErrorResponse.NotFound("User $requestorId does not exist"))
+            return
+        }
+
+        if (requestorUser.role != UserRole.ADMIN) {
+            presenter.presentFailure(ErrorResponse.Unauthorized("User $requestorId is not admin"))
+            return
+        }
+
+        val userId =
+            try {
+                UserId(request.userId)
+            } catch (e: IllegalArgumentException) {
+                presenter.presentFailure(ErrorResponse.BadRequest("Invalid id: ${e.message}"))
+                return
+            }
+
+        val user =
+            try {
+                userGateway.findById(userId)
+            } catch (e: Exception) {
+                presenter.presentFailure(ErrorResponse.InternalServerError("Failed to load user"))
+                logger.error("Failed to load user", e)
+                return
+            }
+
+        if (user == null) {
+            presenter.presentFailure(ErrorResponse.NotFound("User $userId does not exist"))
+            return
+        }
+
+        if (request.role.toDomain() == user.role) {
+            presenter.presentFailure(ErrorResponse.BadRequest("User $userId has already this role"))
+            return
+        }
+
+        val userCopy = user.copy(
+            role = request.role.toDomain(),
+        )
+
+        val saved = try {
+            userGateway.save(userCopy)
+        } catch (e: Exception) {
+            presenter.presentFailure(ErrorResponse.InternalServerError("Failed to save user: ${e.message}"))
+            logger.error("Failed to save user", e)
+            return
+        }
+
+        logger.audit(
+            AuditLog(
+                id = AuditLogId.generate(),
+                message =
+                    AuditLogMessage(
+                        "Changed role of user ${saved.username.value} with id ${saved.id.value} to ${user.role}",
+                    ),
+                performedByUserId = requestorId,
+                createdAt = Instant.now(),
+            ),
+        )
+    }
+}
